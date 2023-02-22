e0c9654af341f6d20b46809cb618f507f21b2118
kafkaphoenix <jaguilerapuerta@gmail.com>
2023-02-22 11:17:49 +0100

    Configuration and error handling

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,24 +1,42 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
-	// router / servermux
-	mux := http.NewServeMux()
+	var cfg config
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	//addr := os.Getenv("SNIPPETBOX_ADDR")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	app := &application{
+		errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+		infoLog:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+	}
 
 	// web server
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     cfg.addr,
+		ErrorLog: app.errorLog,
+		Handler:  app.routes(),
+	}
+
+	app.infoLog.Printf("Starting server on %s", cfg.addr)
+	err := srv.ListenAndServe()
+	app.errorLog.Fatal(err)
 }
