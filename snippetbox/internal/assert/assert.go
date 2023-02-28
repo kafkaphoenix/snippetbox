b1d5c067ebee5595b763a1ebc72e2aa474762830
kafkaphoenix <jaguilerapuerta@gmail.com>
2023-03-01 00:38:48 +0100

    Testing
    - mocked db
    - test db
    - add example tests for handlers, models, middlewares and templates

---
+++ snippetbox/internal/assert/assert.go
@@ -0,0 +1,38 @@
+package assert
+
+import (
+	"strings"
+	"testing"
+)
+
+func Equal[T comparable](t *testing.T, actual, expected T) {
+	t.Helper()
+
+	if actual != expected {
+		t.Errorf("Failed")
+	}
+}
+
+func StringContains(t *testing.T, actual, expectedSubstring string) {
+	t.Helper()
+
+	if !strings.Contains(actual, expectedSubstring) {
+		t.Errorf("got: %q; expected to contain: %q", actual, expectedSubstring)
+	}
+}
+
+func NilError(t *testing.T, actual error) {
+	t.Helper()
+
+	if actual != nil {
+		t.Errorf("got: %v; expected: nil", actual)
+	}
+}
+
+func NoMatchingRecordError(t *testing.T, actual error) {
+	t.Helper()
+
+	if actual.Error() != "models: no matching record found" {
+		t.Errorf("got: %v; expected: models: no matching record found", actual)
+	}
+}
