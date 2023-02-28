a0a80ac446d93e6b0057bae74a986b5c620079c7
kafkaphoenix <jaguilerapuerta@gmail.com>
2023-02-28 11:53:50 +0100

    Security improvements
    - Website uses TLS with autosigned certificate
    - Added iddle, read, write timeout options to the server

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"log"
@@ -61,6 +62,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 	infoLog.Print("Initializing session manager...")
 
 	app := &application{
@@ -74,15 +76,33 @@ func main() {
 
 	defer db.Close()
 
+	// Optimization ONLY elliptic curves with assembly implementations are used.
+	// Strong cipher suites for TLS 1.0-1.2, TLS 1.3 connections' go supports are strong so it ignores this parameter
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		},
+	}
+
 	// web server
 	srv := &http.Server{
-		Addr:     cfg.addr,
-		ErrorLog: app.errorLog,
-		Handler:  app.routes(),
+		Addr:         cfg.addr,
+		ErrorLog:     app.errorLog,
+		Handler:      app.routes(),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	app.infoLog.Printf("Starting server on %s", cfg.addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	app.errorLog.Fatal(err)
 }
 
